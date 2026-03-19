6729c17435a147670b639e79f839d5f93dedf288
Abraham Hamidi <abraham.hamidi01@gmail.com>
2026-03-19 17:26:59 -0500

    Only one SEMVER for protocol and bump patch version

--- src/control/usb_mux.rs
+++ src/control/usb_mux.rs
@@ -1,5 +1,3 @@
-pub const SEMVER: [u16; 3] = [0, 1, 5];
-
 #[cfg_attr(feature = "pyo3", pyo3::pyclass(get_all))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, Copy)]
@@ -31,30 +29,12 @@ pub struct StartPairing {
     pub timeout_ms: u32,
 }
 
-#[repr(C)]
-#[cfg_attr(feature = "pyo3", pyo3::pyclass(get_all))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Copy, Debug)]
-pub struct UsbMuxVersion {
-    pub protocol_semver: [u16; 3],
-    pub firmware_semver: [u16; 3],
-}
-
-impl UsbMuxVersion {
-    pub fn new(firmware_semver: [u16; 3]) -> Self {
-        Self {
-            protocol_semver: SEMVER,
-            firmware_semver,
-        }
-    }
-}
-
 #[cfg_attr(feature = "pyo3", pyo3::pyclass(get_all))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, Debug)]
 pub enum UsbMuxCtrlMsg {
     ReadVersion(),
-    ReadVersionResponse(UsbMuxVersion),
+    ReadVersionResponse(crate::Version),
 
     ListBonds,
     ListBondsResponse(heapless::Vec<super::BondedDevice, { crate::mux::MAX_DEVICES }>),
